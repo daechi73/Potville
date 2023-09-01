5965abe1911e959c746794fb1a189d461fdd0c9f
J <59581793+daechi73@users.noreply.github.com>
2023-09-01 17:29:21 -0400

    added inspiring section with a quote and a button

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,21 @@
 import Image from "../Image";
-import img2 from "../../assets/img.avif";
+import img from "../../assets/img.avif";
+import img4 from "../../assets/img4.jpg";
+import img2Cut from "../../assets/img2Cut.jpg";
+import imgCut from "../../assets/imgCut.jpg";
 import "../../styles/Home.css";
 
 const Home = () => {
   return (
     <>
       <main className="home">
-        <Image className="homeMainImg" image={img2} name={"homeBackground"} />
+        <Image className="homeMainImg" image={imgCut} name={"homeBackground"} />
+        <section className="inspiring">
+          <summary className="home-quote">
+            Where Urban style comes alive
+          </summary>
+          <button className="shopBtn">Shop</button>
+        </section>
       </main>
     </>
   );

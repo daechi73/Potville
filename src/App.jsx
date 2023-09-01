a8a0d8e520adf507256aac80b5d44d409bf39365
J <59581793+daechi73@users.noreply.github.com>
2023-09-01 17:28:40 -0400

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }

fef9ea48546ee5f31de32007be853c6998f78c9a
J <59581793+daechi73@users.noreply.github.com>
2023-09-01 17:33:30 -0400

    added basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        Javui inc© 2023 Urban design since 1204
+      </footer>
+    </>
+  );
+};
+
+export default Footer;

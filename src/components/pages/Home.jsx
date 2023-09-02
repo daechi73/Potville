70ede84f50d4a01baee511e76710046efb051876
J <59581793+daechi73@users.noreply.github.com>
2023-09-01 20:20:07 -0400

    changed button to link

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,6 +4,7 @@ import img4 from "../../assets/img4.jpg";
 import img2Cut from "../../assets/img2Cut.jpg";
 import imgCut from "../../assets/imgCut.jpg";
 import "../../styles/Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -14,7 +15,7 @@ const Home = () => {
           <summary className="home-quote">
             Where Urban style comes alive
           </summary>
-          <button className="shopBtn">Shop</button>
+          <Link to="/shop">Shop</Link>
         </section>
       </main>
     </>

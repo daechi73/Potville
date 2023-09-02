d7a6131f67ef701e8c1a72bcfb73163d31859ccf
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 19:02:50 -0400

    added className to img

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,12 @@
+import "../styles/ProductCard.css";
+
 const ProductCard = (props) => {
   return (
     <>
       <img
+        className="card-img"
         src={props.product.image}
         alt={props.product.title}
-        height={300}
-        width={300}
       />
     </>
   );

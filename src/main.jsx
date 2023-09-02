705a7c8d1393768f25780f5072ec65e5e1b79142
J <59581793+daechi73@users.noreply.github.com>
2023-09-01 20:50:38 -0400

    added errorpage

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   { path: "/:name", element: <App /> },
 ]);

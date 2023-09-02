f73f263f97115628595aa4d349340cace1e21b50
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 18:51:16 -0400

    added simple about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,12 @@
 const About = () => {
-  return <>About</>;
+  return (
+    <>
+      Javui is all about leading urban style with originality that cannot be
+      recreated. With our deep root stemming out from as early as year 1205,
+      from kings to peasants we provided high-quality, on trend clothing that
+      empowers you to look and feel your best every day.
+    </>
+  );
 };
 
 export default About;

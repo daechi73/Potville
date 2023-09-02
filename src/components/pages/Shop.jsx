8081085c7cbadfde8795e94c50e0c82fd33d2689
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 18:36:50 -0400

    added productNav category title; added down right turn arrow to each list

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -3,32 +3,39 @@ import "../../styles/Shop.css";
 import { Link } from "react-router-dom";
 
 const Shop = (props) => {
+  console.log("rendering Shop");
   return (
     <>
       <section className="shop">
         <nav className="productNav">
+          <div className="category-title">Category:</div>
           <ul className="product-category">
             <li>
+              <span>&#10551;</span>
               <Link to="/shop" onClick={props.dataFetchSwitch}>
                 All Products
               </Link>
             </li>
             <li>
+              <span>&#10551;</span>
               <Link to="/shop/electronics" onClick={props.dataFetchSwitch}>
                 Electronics
               </Link>
             </li>
             <li>
+              <span>&#10551;</span>
               <Link to="/shop/jewelery" onClick={props.dataFetchSwitch}>
                 Jewelery
               </Link>
             </li>
             <li>
+              <span>&#10551;</span>
               <Link to="/shop/men's clothing" onClick={props.dataFetchSwitch}>
                 Men's Clothing
               </Link>
             </li>
             <li>
+              <span>&#10551;</span>
               <Link to="/shop/women's clothing" onClick={props.dataFetchSwitch}>
                 Women's Clothing
               </Link>

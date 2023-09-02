45375bcd76a60b7588df369b7c9fd8a32791e5dc
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 15:11:06 -0400

    moved dataFetchSwitch onclick from nome navlink to shop navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = (props) => {
         <div className="header-name">Javui</div>
         <ul className="navLinks">
           <li>
-            <Link to="/" onClick={props.dataFetchSwitch}>
-              Home
-            </Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/shop">Shop</Link>
+            <Link to="/shop" onClick={props.dataFetchSwitch}>
+              Shop
+            </Link>
           </li>
           <li>
             <Link to="/about">About</Link>

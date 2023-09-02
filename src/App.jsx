bc0e0a65adfab25761d9939ee0c289b766b8df04
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 18:35:08 -0400

    debug console added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const dataFetchSwitch = () => {
     setDataFetch(!dataFetch);
   };
+  console.log("renderingAPP");
   return (
     <>
       <Header dataFetchSwitch={dataFetchSwitch} />

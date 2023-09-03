846e2942f9a1b665dff65739d769758416906d1f
J <59581793+daechi73@users.noreply.github.com>
2023-09-02 21:09:26 -0400

    added more to about content & styled it

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,16 @@
+import "../../styles/About.css";
+
 const About = () => {
   return (
-    <>
-      Javui is all about leading urban style with originality that cannot be
-      recreated. With our deep root stemming out from as early as year 1205,
-      from kings to peasants we provided high-quality, on trend clothing that
-      empowers you to look and feel your best every day.
-    </>
+    <section className="about">
+      <p className="about-mission">
+        <i className="about-javui">Javui</i> is all about leading urban style
+        with originality that cannot be recreated. With our deep root stemming
+        out from as early as year 1205, from kings to peasants we've provided
+        high-quality, on trend clothing that empowers you to look and feel your
+        best every day.
+      </p>
+    </section>
   );
 };
 

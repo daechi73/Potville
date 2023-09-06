b5953b3d8b5eaa8506a0d1a341ccd885c9bef8e1
J <59581793+daechi73@users.noreply.github.com>
2023-09-06 01:10:55 -0400

    product info is displayed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,14 +2,28 @@ import "../styles/ProductCard.css";
 import Image from "./Image";
 
 const ProductCard = (props) => {
+  console.log(props.product);
   return (
-    <>
+    <div className="productCard">
       <Image
         className="card-img"
         image={props.product.image}
         name={props.product.title}
       />
-    </>
+      <div className="productCard-infos">
+        {" "}
+        <div className="productCard-name productCard-info">
+          {" "}
+          {props.product.title}
+        </div>
+        <div className="productCard-price productCard-info">
+          Price: ${props.product.price}
+        </div>
+        <div className="productCard-rating productCard-info">
+          Rating: {props.product.rating.rate}
+        </div>
+      </div>
+    </div>
   );
 };
 

464f3588d7e1467ec85afffba6b45e73f0767f01
J <59581793+daechi73@users.noreply.github.com>
2023-09-07 22:58:58 -0400

    added keys to each stars iterated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,32 @@
 import "../styles/ProductCard.css";
+import { useState } from "react";
 import Image from "./Image";
 
 const ProductCard = (props) => {
-  console.log(props.product);
+  //console.log(props.product);
+  const [itemQuantity, setItemQuantity] = useState(0);
+  let productId = 0;
+
   const reviewStars = () => {
     const reviewStars = [];
     for (let i = 0; i < Math.floor(props.product.rating.rate); i++) {
-      const star = <span className="fa fa-star checked"></span>;
+      const star = <span key={i} className="fa fa-star checked"></span>;
       reviewStars.push(star);
     }
     if (props.product.rating.rate % 1 !== 0)
-      reviewStars.push(<span className="fa fa-star"></span>);
-    reviewStars.push(<span>{props.product.rating.count}</span>);
+      reviewStars.push(
+        <span key={reviewStars.length} className="fa fa-star"></span>
+      );
+    reviewStars.push(
+      <span key={reviewStars.length + 1}>{props.product.rating.count}</span>
+    );
     return reviewStars;
   };
 
+  const itemQuantityAdjuster = (e) => {
+    if (e.target.textContent === "+") setItemQuantity(itemQuantity + 1);
+    if (e.target.textContent === "-") setItemQuantity(itemQuantity - 1);
+  };
   return (
     <div className="productCard">
       <Image
@@ -35,6 +47,15 @@ const ProductCard = (props) => {
           Rating: {props.product.rating.rate}
         </div>
         {reviewStars()}
+      </div>
+      <div className="quantityContainer">
+        <span className="decrementQuan" onClick={itemQuantityAdjuster}>
+          -
+        </span>
+        <span className="quantity" onClick={itemQuantityAdjuster}>
+          {itemQuantity}
+        </span>
+        <span className="incrementQuan">+</span>
       </div>
       <button className="addToCartBtn">Add to Cart</button>
     </div>

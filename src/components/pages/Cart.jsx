a1e9fdaa517c743cda30b8d43456d943ee24a200
J <59581793+daechi73@users.noreply.github.com>
2023-09-08 22:54:02 -0400

    returns ProductCartCard

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,5 +1,13 @@
-const Cart = () => {
-  return <>CART</>;
+import ProductCartCard from "../ProductCartCard";
+
+const Cart = (props) => {
+  console.log(props.cart);
+  const renderCart = () => {
+    return props.cart.map((product) => (
+      <ProductCartCard key={product.id} product={product} />
+    ));
+  };
+  return <>{renderCart()}</>;
 };
 
 export default Cart;

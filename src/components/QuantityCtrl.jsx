85a4830be3706f5745ef34c0bad92d3b923f03f1
J <59581793+daechi73@users.noreply.github.com>
2023-09-09 00:54:02 -0400

    the controller will update cart only in ProductCartCard component

--- src/components/QuantityCtrl.jsx
+++ src/components/QuantityCtrl.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 
 const QuantityCtrl = (props) => {
+  const updateItemQuantity = (e) => {
+    let newItemQuantity;
+    if (e.target.textContent === "+") newItemQuantity = props.itemQuantity + 1;
+    else newItemQuantity = props.itemQuantity - 1;
+    if (newItemQuantity === 0)
+      return alert("Cannot add 0 quantity of this item");
+    let cartTemp = props.cart.map((cartProduct) => {
+      if (cartProduct.id === props.product.id) {
+        return {
+          ...cartProduct,
+          quantity: newItemQuantity,
+        };
+      } else return cartProduct;
+    });
+    props.setCart(cartTemp);
+    console.log(cartTemp);
+  };
   const itemQuantityAdjuster = (e) => {
     if (e.target.textContent === "+")
       props.setItemQuantity(props.itemQuantity + 1);
     if (e.target.textContent === "-")
-      if (itemQuantity === 0) return;
+      if (props.itemQuantity === 0) return;
       else props.setItemQuantity(props.itemQuantity - 1);
   };
-
+  const clickHandler = (e) => {
+    itemQuantityAdjuster(e);
+    if (props.productCartCard) updateItemQuantity(e);
+  };
   return (
     <div className="quantityController">
-      <span className="decrementQuan" onClick={itemQuantityAdjuster}>
+      <span className="decrementQuan" onClick={clickHandler}>
         -
       </span>
       <span className="quantity">{props.itemQuantity}</span>
-      <span className="incrementQuan" onClick={itemQuantityAdjuster}>
+      <span className="incrementQuan" onClick={clickHandler}>
         +
       </span>
     </div>

564b730c071a3956a34cf5dbb63d79fb38efdea3
J <59581793+daechi73@users.noreply.github.com>
2023-09-10 01:26:53 -0400

    added a container for Image component

--- src/components/ProductCartCard.jsx
+++ src/components/ProductCartCard.jsx
@@ -8,12 +8,14 @@ const ProductCartCard = (props) => {
   return (
     <>
       <div className={"productCartCard " + props.product.title}>
-        <Image
-          className="cart-card-img"
-          image={props.product.image}
-          name={props.product.title}
-        />
-        <div className="productCartCard-infos">
+        <div className="cart-img">
+          <Image
+            className="cart-card-img"
+            image={props.product.image}
+            name={props.product.title}
+          />
+        </div>
+        <div className="cart-infos">
           <div className="productCard-name productCard-info">
             {props.product.title}
           </div>

753d06a63a0e2364c4887e6c7a00cad629952170
J <59581793+daechi73@users.noreply.github.com>
2023-09-10 01:01:11 -0400

    header, info, and total

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import "../styles/Invoice.css";
 
 const Invoice = (props) => {
   console.log(props.cart);
-
+  let subTotal = 0;
   const displayCartInfo = () => {
     return props.cart.map((item) => {
+      const itemTotal = Math.round(item.price * item.quantity * 100) / 100;
+      subTotal += itemTotal;
       return (
         <>
           {item.quantity ? (
-            <li key={item.id}>
-              <span className="invoice-item-info id">id:{item.id}</span>
-              <span className="invoice-item-info price">{item.price}</span>
-              <span className="operator">X</span>
-              <span className="invoice-item-info quantity">
-                {item.quantity}
-              </span>
-              <span className="invoice-item-info total">
-                {Math.round(item.price * item.quantity * 100) / 100}
-              </span>
+            <li key={item.id} className="invoice-list">
+              <div className="invoice-item-info id">{item.id}</div>
+              <div className="invoice-item-info price">${item.price}</div>
+              <div className="operator"></div>
+              <div className="invoice-item-info quantity">{item.quantity}</div>
+              <div className="invoice-item-info total-item">${itemTotal}</div>
             </li>
           ) : (
             ""
@@ -29,7 +28,21 @@ const Invoice = (props) => {
 
   return (
     <div className="invoice">
-      <ul>{displayCartInfo()}</ul>
+      <div className="invoice-header">Invoice</div>
+      <div className="invoice-info">
+        <div className="invoice-heading">
+          <div>id</div>
+          <div>P</div>
+          <div></div>
+          <div>Q</div>
+          <div>T</div>
+        </div>
+        <ul className="invoice-ul">{displayCartInfo()}</ul>
+      </div>
+
+      <div className="invoice-total">
+        Total: ${Math.round(subTotal * 100) / 100}
+      </div>
     </div>
   );
 };

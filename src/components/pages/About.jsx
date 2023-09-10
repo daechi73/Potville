cafed3c4fe2bab882a74e81f91105986a3fb6cb3
J <59581793+daechi73@users.noreply.github.com>
2023-09-10 01:30:40 -0400

    fixed grammer

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
     <section className="about">
       <p className="about-mission">
         <i className="about-javui">Javui</i> is all about leading urban style
-        with originality that cannot be recreated. With our deep root stemming
-        out from as early as year 1205, from kings to peasants we've provided
-        high-quality, on trend clothing that empowers you to look and feel your
-        best every day.
+        with originality that cannot be recreated. With our deep roots stemming
+        back as early as the year 1205, from kings to peasants, we've provided
+        high-quality, on-trend clothing that empowers you to look and feel your
+        best every day."
       </p>
     </section>
   );
